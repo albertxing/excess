3c3606906173bf6c350c07b4c27069b8281d60b7
Albert Xing <albert.xing@alumni.ubc.ca>
2014-09-19 21:05:13 -0700

    More improvements

--- background.js
+++ background.js
@@ -14,25 +14,23 @@ function installed (details) {
 	update();
 }
 
-function update () {
+function update (callback) {
 	chrome.identity.getAuthToken({interactive: true}, function (token) {
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', apiUrl + 'files');
+		var query = 'title = "Excess" and mimeType = "application/vnd.google-apps.folder"';
+		xhr.open('GET', apiUrl + 'files?q=' + query);
 		xhr.setRequestHeader('Authorization', 'Bearer ' + token);
 		xhr.onload = function () {
 			var data = JSON.parse(xhr.responseText);
-			var ta = data.items.filter(function (i) {
-				return (i.mimeType == 'application/vnd.google-apps.folder'
-					&& i.title == 'Excess');
-			});
+			var folder = data.items[0];
 
-			if (!ta.length) {
+			if (!folder) {
 				var fxhr = new XMLHttpRequest();
 				fxhr.open('POST', apiUrl + 'files');
 				fxhr.setRequestHeader('Authorization', 'Bearer ' + token);
 				fxhr.setRequestHeader('Content-Type', 'application/json');
 				fxhr.onload = function () {
-					ta = JSON.parse(fxhr.responseText).items.filter(function (i) {
+					folder = JSON.parse(fxhr.responseText).items.filter(function (i) {
 						return (i.mimeType == 'application/vnd.google-apps.folder'
 							&& i.title == 'Excess');
 					});
@@ -46,34 +44,55 @@ function update () {
 				fxhr.send(JSON.stringify(folderData));
 			}
 
-			var folder = ta[0];
 			chrome.storage.local.set({'folder': folder});
 
-			var query = '\'' + folder.id + '\' in parents';
+			var query = '"' + folder.id + '" in parents and trashed = false';
 			var cxhr = new XMLHttpRequest();
 			cxhr.open('GET', apiUrl + 'files?q=' + query);
 			cxhr.setRequestHeader('Authorization', 'Bearer ' + token);
 			cxhr.onload = function () {
 				var files = JSON.parse(cxhr.responseText);
 				var local;
-				chrome.storage.local.get('files', function (local) {
-					console.log(files);
-					console.log(local);
+				chrome.storage.local.get('files', function (result) {
+					var local = result.files;
 
+					Object.keys(local).filter(function (id) {
+						return files.items.filter(function (f) {
+							return f.id == id;
+						}).length == 0;
+					}).forEach(function (id) {
+						delete local[id];
+					});
+
+					var incoming = 0;
 					files.items.forEach(function (item) {
 						if (!local[item.id] || local[item.id].version != item.version) {
+							incoming++;
+
 							dxhr = new XMLHttpRequest();
 							dxhr.open('GET', item.downloadUrl);
 							dxhr.setRequestHeader('Authorization', 'Bearer ' + token);
 							dxhr.onload = function () {
 								var raw = dxhr.responseText;
-								item.raw = raw;
+								item.raw = raw.trim();
+
+								incoming--;
+								if (incoming == 0) {
+									chrome.storage.local.set({files: local});
+
+									if (callback) callback(local);
+								}
 							}
 
 							dxhr.send();
 							local[item.id] = item;
 						}
 					});
+
+					if (incoming == 0) {
+						chrome.storage.local.set({files: local});
+						if (callback) callback(local);
+					}
 				});
 			}
 
@@ -83,3 +102,10 @@ function update () {
 		xhr.send();
 	});
 }
+
+chrome.runtime.onConnect.addListener(function (port) {
+	port.onMessage.addListener(function (msg) {
+		if (msg == 'update')
+			update(function (files) {port.postMessage(files)});
+	});
+});
--- options.js
+++ options.js
@@ -1,3 +1,8 @@
+var port = chrome.runtime.connect();
+port.onMessage.addListener(function (msg) {
+	update(msg);
+});
+
 var folder;
 chrome.storage.local.get('folder', function (data) {
 	folder = data.folder;
@@ -17,6 +22,33 @@ var rules = document.querySelector('.rules pre')
 var save = document.querySelector('.options .save');
 var remove = document.querySelector('.options .remove');
 
+function update (local) {
+	var files = local;
+	if (!files) {
+		chrome.storage.local.get('files', function (data) {
+			files = data.files;
+			list(files);
+		});
+	}
+	list(files);
+}
+
+function list (files) {
+	styles.innerHTML = '';
+
+	for (var id in files) {
+		var li = document.createElement('li');
+		var file = files[id];
+		li.classList.add('link');
+		li.innerHTML = file.description;
+		li.file = file;
+		li.onclick = function () {
+			load(this.file);
+		};
+		styles.appendChild(li);
+	}
+}
+
 newl.onclick = function () {
 	title.innerHTML = 'New Style';
 	title.classList.add('dirty');
@@ -29,15 +61,16 @@ save.onclick = function () {
 	if (!state) {
 		var li = document.createElement('li');
 		li.classList.add('link');
-		li.innerHTML = title.innerHTML;
+		li.innerHTML = title.textContent;
 		styles.appendChild(li);
 
 		chrome.identity.getAuthToken({interactive: true}, function (token) {
 			var css = editor.getValue();
 			var metadata = {
-				title: title.textContent.toLowerCase() + '.css',
+				title: title.textContent.toLowerCase().replace(/\s/, '_') + '.css',
 				mimeType: 'text/css',
 				parents: [folder],
+				description: title.textContent,
 				properties: [{
 					key: 'match',
 					value: rules.textContent,
@@ -61,7 +94,47 @@ save.onclick = function () {
 			xhr.setRequestHeader('Content-Size', size);
 
 			xhr.onload = function () {
-				console.log(xhr.responseText);
+				var file = JSON.parse(xhr.responseText);
+				file.raw = css;
+				chrome.runtime.sendMessage('update');
+			};
+
+			xhr.send(req);
+		});
+	} else {
+		chrome.identity.getAuthToken({interactive: true}, function (token) {
+			var css = editor.getValue().trim();
+			var metadata = {
+				title: title.textContent.toLowerCase().replace(/\s/, '_') + '.css',
+				description: title.textContent,
+				properties: [{
+					key: 'match',
+					value: rules.textContent,
+					visibility: 'PRIVATE'
+				}]
+			};
+			var metastring = JSON.stringify(metadata);
+
+			// var formreq = '--excess\nContent-Type: application/x-www-form-urlencoded\n\nuploadType=multipart';
+			var metareq = '--excess\nContent-Type: application/json\n\n' + metastring;
+			var cssreq = '\n\n--excess\nContent-Type: text/css\n\n' + css;
+			var end = '\n\n--excess--';
+
+			var req = metareq + cssreq + end;
+			var size = req.length;
+
+			var xhr = new XMLHttpRequest();
+			xhr.open('PUT', 'https://www.googleapis.com/upload/drive/v2/files/' + state.id + '?uploadType=multipart');
+			xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+			xhr.setRequestHeader('Content-Type', 'multipart/related; boundary="excess"');
+			xhr.setRequestHeader('Content-Size', size);
+
+			xhr.onload = function () {
+				var file = JSON.parse(xhr.responseText);
+				file.raw = css;
+				port.postMessage('update');
+
+				state = file;
 			};
 
 			xhr.send(req);
@@ -70,3 +143,15 @@ save.onclick = function () {
 
 	title.classList.remove('dirty');
 }
+
+function load (file) {
+	title.innerHTML = file.description;
+	rules.innerHTML = file.properties.filter(function (p) {
+		return p.key == 'match';
+	})[0].value;
+	main.style.display = 'initial';
+	editor.setValue(file.raw.trim());
+	state = file;
+}
+
+update();
